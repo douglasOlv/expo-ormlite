4a1c3e74b5ed1dfda46353cab8ce4466e6ec9031
Douglas Oliveira <douglas4030@gmail.com>
2022-02-20 12:11:40 -0300

    fix: update index file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { OrmLite } from './OrmLite';
 import { Field } from './attributes';
-import { Schema } from './schema';
+import { Schema } from './Schema';
 import { Repository } from './Repository';
 
 export { OrmLite, Repository, Schema, Field };
